fb60e0a8b3bfe997a79d3f3d05f73610bfe4a174
mura0717 <mura0717@stud.kea.dk>
2023-03-05 12:34:12 +0100

    week5Day1

---
+++ day1_ex5.js
@@ -0,0 +1,11 @@
+function div(n1, n2){
+  return n1 / n2;
+}
+
+
+const cb = function(n1,n2, callback){
+  return "Result from the two numbers: "+n1+"+"+n2+"="+callback(n1,n2);
+}
+  
+
+console.log(cb(2,2,mul));
