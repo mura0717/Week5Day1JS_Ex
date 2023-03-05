65fadfd9ad698e8e23b0cd9e804cdd7dff90c48e
mura0717 <mura0717@stud.kea.dk>
2023-03-05 14:29:54 +0100

    week5Day1

--- day1_exFilter.js
+++
@@ -1,4 +0,0 @@
-
-const members = ['Lars', 'Jan', 'Peter', 'Bo', 'Frederik'];
-const shortNames = members.filter(member => member.length <= 3);
-console.log(shortNames);
--- day1_exForEach.js
+++
@@ -1,5 +0,0 @@
-const members = ['Lars', 'Jan', 'Peter', 'Bo', 'Frederik'];
-members.forEach(member => console.log(member));
-console.log("------------------------");
-shortNames.forEach(member => console.log(member));
-console.log("------------------------");
